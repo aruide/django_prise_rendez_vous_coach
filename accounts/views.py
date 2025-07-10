7c187b04499727d80fb2ce4cef39b245d8caec9e
Aruide <ruide.aurelien@gmail.com>
2025-07-10 14:46:11 +0200

    modify: modif code

--- accounts/views.py
+++ accounts/views.py
@@ -1,37 +1,14 @@
 # accounts/views.py
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
-from django.contrib.auth.models import Group
-from django.urls import reverse
-from django.utils.http import urlsafe_base64_decode
-from django.contrib.auth.tokens import default_token_generator
 from .forms import CustomSignupForm
 from django.contrib.auth import get_user_model
-from django.core.mail import EmailMultiAlternatives
 from django.contrib.auth.decorators import login_required
 from core.decorators import logout_required
 
 from django.utils.decorators import method_decorator
 from django.contrib.auth import views as auth_views
 
-
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.utils.http import urlsafe_base64_decode
-from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import get_user_model
-from django.utils.decorators import method_decorator
-from django.contrib.auth import views as auth_views
-
-from .forms import CustomSignupForm
-from core.decorators import logout_required
 from .services.user_services import create_inactive_user_with_group, generate_activation_url, activate_user_from_token, get_user_role
 from .services.email_services import send_activation_email
 
